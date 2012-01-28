d91b427a288b533d134f88f0e548f12b4a944b8b
Souichi Saitou <souichi.saitou@gmail.com>
2012-01-28 19:19:35 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sukesan::Application.config.secret_token = 'b61b76389c018efdbbc77076a8d76a68a9e396d39d0e55ec39fa21145f9eb2a354f9d18713999f943b67f1e736665640944fdfcf5df1a829608cbcc66a7b3804'
